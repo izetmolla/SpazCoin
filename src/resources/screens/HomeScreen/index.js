f3c9c4e1f808c1cc4c40be699db0d8334861df72
Izet Molla <izetmolla@gmail.com>
2020-10-15 00:53:07 +0200

    mm

--- src/resources/screens/HomeScreen/index.js
+++ src/resources/screens/HomeScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, FlatList, StyleSheet, Text } from 'react-native';
+import { View, FlatList, StyleSheet, Text, Button } from 'react-native';
 
 import { AuthContext } from '../../../app/contexts/AuthContext';
 import { ThemeContext } from '../../../app/contexts/ThemeContext';
@@ -7,11 +7,11 @@ import { ThemeContext } from '../../../app/contexts/ThemeContext';
 export function HomeScreen({ navigation, route }) {
     const { logout } = React.useContext(AuthContext);
     const switchTheme = React.useContext(ThemeContext);
-    console.log(route)
     return (
         <View style={{ flex: 1, justifyContent: "center", alignContent: "center", alignItems: "center" }}>
             <Text style={{ fontWeight: "bold", fontSize: 25 }}>Home Screen</Text>
             {/* <Text>{route.params.type}</Text> */}
+            <Button title="Logout" onPress={() => logout()} />
         </View>
     );
 }
--- src/resources/screens/RedirectingScreen/index.js
+++ src/resources/screens/RedirectingScreen/index.js
@@ -4,13 +4,16 @@ import { View, FlatList, StyleSheet, Text } from 'react-native';
 import { AuthContext } from '../../../app/contexts/AuthContext';
 import { ThemeContext } from '../../../app/contexts/ThemeContext';
 
-export function RedirectingScreen({ navigation }) {
+export function RedirectingScreen({ navigation, route }) {
     const { logout } = React.useContext(AuthContext);
     const switchTheme = React.useContext(ThemeContext);
-
+    console.log(route)
     return (
         <View style={{ flex: 1, justifyContent: "center", alignContent: "center", alignItems: "center" }}>
-            <Text style={{fontWeight: "bold", fontSize: 25}}>Redirecting Screen</Text>
+            <Text style={{ fontWeight: "bold", fontSize: 25 }}>Redirecting Screen</Text>
+            <Text>{route.params.type}</Text>
+            <Text>{route.params.data}</Text>
+
         </View>
     );
 }
